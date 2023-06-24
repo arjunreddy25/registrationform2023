58519f6be2bf66b0bae57a6c13d318cf972c4d7b
arjunreddy25 <ar1686153@gmail.com>
2023-06-24 17:34:46 +0530

    CHANGES

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User,auth
 
 # Create your views here.
+def signupview(request):
+    if request.method== 'POST':
+        teamname = request.POST['teamname']
+        phone = request.POST['phone']
+        email = request.POST['email']
+        password = request.POST['password']
+        
+        data = User.objects.create_user(teamname=teamname,phone=phone,email=email,password=password)
+        data.save()
+        return redirect('login')
+
+    return render(request,'signup.html')
+ 
+def Login(request):
+    if request.method=='POST':
+        email = request.POST['email']
+        password = request.POST['password']
+        user = auth.authenticate(email=email,password=password)
+        
+        if user is not None:
+            auth.login(request,user)
+            return render('next')
+
+    return render(request,'login.html')
+
--- registrationform/urls.py
+++ registrationform/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from myapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('signup/',views.signupview,name='signup'),
+    path('login/',views.Login,name='login')
 ]
